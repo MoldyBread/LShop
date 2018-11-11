9f2ad1971c89ae4738667bd9c633217d93ee9a99
Dan <lyash.dv@gmail.com>
2018-11-11 15:59:20 +0200

    footer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './scss/main.scss';
 console.log('Hello!');
 console.log(`The time is ${new Date()}`);
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+$.get( "http://nit.tron.net.ua/api/category/list", function( data ) {
+  $( ".result" ).html( data );
+  $("#first").text(data);
+});
