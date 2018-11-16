978988cee321f001e01a0402f6ef3c48a3522d45
Dan <lyash.dv@gmail.com>
2018-11-17 00:42:01 +0200

    Few fixes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ console.log(`The time is ${new Date()}`);
 
 var selectedCategory = 1;
 
+var categoryDescription=[];
 
 $( document ).ready(function() {
     console.log( "ready!" );
@@ -28,10 +29,19 @@ function loadGoods(id){
 
         	out+='<div class="goods-text">';
         	out+='<p>'+data[key]['name']+'</h3>';
-        	out+='<p>'+data[key]['price']+'</p>';
+        	if(data[key]['special_price']===null){
+        		out+='<p class="reg-price">'+data[key]['price']+' грн. </p>';
+        	}
+        	else{
+        		out+='<p class="sales">'+data[key]['price']+' грн. </p>';
+        		out+='<p class="price">'+data[key]['special_price']+' грн. </p>';
+        	}
+        	
+        	out+='<button class="my-button"> В корзину </button>'
         	out+='</div>';
 
         	out+='</div>';
+
         }
         $('#goods').html(out);
     });
@@ -42,13 +52,18 @@ function loadCategories() {
 		var out='<ul>';
 		 for(var key in data){
 		 	out+='<li class="load" data-art="'+data[key]['id']+'">'+data[key]['name']+'</li>';
+		 	categoryDescription.push(data[key]['description']);
         }
         out+='</ul>'
+
         $('#categories').html(out);
+        $('#ghead').html(categoryDescription[0]);
+        $('.load:eq(0)').addClass('highlight');
         $('li.load').on('click', function(){
         	var id = $(this).attr('data-art');
 	        console.log(id);
 	        loadGoods(id);
+	        $('#ghead').html(categoryDescription[id-1]);
 	        $('.load:eq('+(selectedCategory-1)+')').removeClass('highlight');
 	        $('.load:eq('+(id-1)+')').addClass('highlight');
 	        selectedCategory=id;
